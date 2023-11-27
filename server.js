3283d139487735493c11dc551fbba97cfeebe996
Failur rahuman <100862900+FAILU026@users.noreply.github.com>
2023-11-27 20:54:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
   {
-    origin: ["http://localhost:3000"],
+    origin: ["https://survey-form-ten-delta.vercel.app"],
     methods: ["POST", "GET"],
     credentials: true
   }
